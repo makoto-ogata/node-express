eb3f051c8830a09a642724fda9bd64f98a887905
Makoto Ogata <ogatamakoto1976@gmail.com>
2022-05-15 23:07:04 +0900

    add post method

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const fs = require('fs');
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/autumn", function(req, res){
+    fs.writeFile(__dirname + "/data.txt", req.body.activity, function(){
+        res.send("投稿完了");
+    });
+});
+
 app.listen(5005,function(){
     console.log("Listening on localhost port 5005");
 });
-
 
 
